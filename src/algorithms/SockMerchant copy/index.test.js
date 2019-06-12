8bc45ea4a977d415ff67ee7e95d19c35106d2211
anish.m.prasad <anish.m.prasad@gmail.com>
2019-06-12 23:03:01 +0530

    update

--- src/algorithms/SockMerchant copy/index.test.js
+++ src/algorithms/SockMerchant copy/index.test.js
@@ -1,5 +1,5 @@
 const SockMerchant = require('./index');
 
-test('SockMerchant', () => {
+test('SockMerchantcopy', () => {
 	expect(SockMerchant(9, [10, 20, 20, 10, 10, 30, 50, 10, 20])).toBe(3);
 });
