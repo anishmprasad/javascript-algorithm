a22056f0ed30cb5c678f9593d6d5a35c45c73d19
anish.m.prasad <anish.m.prasad@gmail.com>
2019-06-21 23:42:21 +0530

    amex update

---
+++ src/algorithms/amex/index.js
@@ -0,0 +1,17 @@
+<div id='brinza-task-description'>
+	<p>
+		Write a function <tt style='white-space:pre-wrap'>solution</tt> that, given an integer N, returns the smallest
+		number with the same number of digits. You can assume N is between 1 and 10<sup>9</sup> (a billion).
+	</p>
+	<p>
+		For example, given N = 125, the function should return 100. Given N = 10, the function should return 10. Given N
+		= 1, the function should return 0.
+	</p>
+</div>;
+
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
+
+function solution(N) {
+	// write your code in JavaScript (Node.js 8.9.4)
+}
