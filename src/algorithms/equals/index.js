1100346c75eb23ff2745910c1c98cc4c48dba7c6
anish.m.prasad <anish.m.prasad@gmail.com>
2019-06-25 21:11:44 +0530

    code update

--- src/algorithms/equals/index.js
+++ src/algorithms/equals/index.js
@@ -1,4 +1,10 @@
+// equals(): Given two values, equals performs a deep comparison between them to figure out if they are the same or not. The expected implementation needs to handle string, numbers, dates, regex, booleans, objects and arrays.
+// equals(1, 1) //=> true
+// equals(1, '1') //=> false
+// equals([1, 2, 3], [1, 2, 3]) //=> true
+
 function equals(a, b) {
+	console.log(a.constructor);
 	if (a.constructor === Array && b.constructor === Array) {
 		return a.every(a => {
 			console.log({ a });
