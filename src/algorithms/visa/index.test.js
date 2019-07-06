1fa400a95e5e033d75763842c937dec2793df5e9
anish.m.prasad <anish.m.prasad@gmail.com>
2019-07-06 21:46:34 +0530

    code update

---
+++ src/algorithms/visa/index.test.js
@@ -0,0 +1,5 @@
+const SockMerchant = require('./index');
+
+test('SockMerchant', () => {
+	expect(SockMerchant(9, [10, 20, 20, 10, 10, 30, 50, 10, 20])).toBe(3);
+});
--- src/algorithms/visa/weirdFaculity/index.test.js
+++ src/algorithms/visa/weirdFaculity/index.test.js
@@ -1,1 +1,6 @@
 // https://fizzbuzzer.com/weird-faculty/
+const SockMerchant = require('./index');
+
+test('SockMerchant', () => {
+	expect(SockMerchant(9, [10, 20, 20, 10, 10, 30, 50, 10, 20])).toBe(3);
+});
