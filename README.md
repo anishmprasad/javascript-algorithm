f2659e6425c629b63fff5eb1891a9d4187f2c146
Anish Prasad <anish.m.prasad@gmail.com>
2025-01-31 16:08:35 +0530

    balloon update

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 - [ ] Sock Merchant
 
+## Sample Image
+
+![Balloon](images/balloon.png)
+![Maze](images/maze.png)
+
+
 ## Licence 
 
 MIT
--- src/examples/algo-demo/src/Page/Balloon/index.tsx
+++ src/examples/algo-demo/src/Page/Balloon/index.tsx
@@ -5,9 +5,9 @@ export default function Baloon(){
     const [count, setCount] = useState(0)
     return (
         <>
-            <h4>Baloon POP</h4>
+            <h4>Balloon POP</h4>
             <p>Use <code>Enter or Space</code> for fire arrow</p>
-            <p>Baloons popped : {count}</p>
+            <p>Balloons popped : {count}</p>
             <BalloonPop count={() => setCount((count) => count + 1)}/>
         </>
     )
